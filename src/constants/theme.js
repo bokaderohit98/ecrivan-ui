08028875fb86a31bce88ca8785f7ec3bf403c084
bokaderohit98 <bokaderohit98@gmail.com>
2019-10-13 19:28:25 +0530

    feat(theming): added color palette

---
+++ src/constants/theme.js
@@ -0,0 +1,10 @@
+export default {
+    palette: {
+        primary: '#1F1B24',
+        secondary: '1F1A24',
+        accent: '#FF0266',
+        primaryText: '#FFFFFF',
+        secondaryText: '#C0C0C0',
+        darkText: '#1F1B24'
+    }
+};
